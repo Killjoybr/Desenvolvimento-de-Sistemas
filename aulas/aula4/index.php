97a596da2aaf9ad90cd81d43ac36f2d7345bb7ed
Killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-03-17 10:29:20 -0300

    init codigo da aula 4 - revisando aula 3

---
+++ aulas/aula4/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 4</title>
+</head>
+<body>
+    <form method="post" action="./soma.php">
+        N1: <input type="number" name="n1">
+        <br><br>
+        N2: <input type="number" name="n2">
+        <br><br>
+        <input type="submit" value="Somar">
+    </form>
+</body>
+</html>
+
+<!-- 
+    TODO: Fazer as outras operações da calculadora
+-->
---
+++ aulas/aula4/soma.php
@@ -0,0 +1,4 @@
+<?php
+    $resultado = $_POST['n1'] + $_POST['n2'];
+    echo '<h1>O resultado eh: ', $resultado,' </h1>'
+?>
