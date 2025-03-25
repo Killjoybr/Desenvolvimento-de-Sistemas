a87a526ffc875b2f697b5127960b2ce3114679c0
Killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-03-25 16:12:49 -0300

    removendo campo id do formulario

--- aulas/aula5/index.php
+++ aulas/aula5/index.php
@@ -46,6 +46,7 @@
                         <td>'.$value['senha'].'</td>
                         <td>'.
                             '<a href="excluir.php?id='.$value['id'].'">Excluir</a>
+                            <a href="editar.php?id='.$value['id'].'">Editar</a>
                         </td>                   
                     </tr>
                     ';
@@ -54,8 +55,6 @@
     </table>
     <form method="post" action="cadastro.php">
       <h2>Cadastro de Usuário</h2>
-      <input type="number" name="id" placeholder="ID">
-      
       <input type="text" placeholder="Jõaozinho da Goiaba" name="nome" required>
       <br><br>
       <input type="email" placeholder="jão@email.com" name="email" required>
