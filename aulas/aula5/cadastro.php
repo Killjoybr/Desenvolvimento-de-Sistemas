2ef0e562f999146f9fa0feae5da79e4de12d3cf8
Killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-03-25 16:11:33 -0300

    removendo logica de atualizacao

--- aulas/aula5/cadastro.php
+++ aulas/aula5/cadastro.php
@@ -1,19 +1,11 @@
 <?php
   require_once 'conexao.php';
   
-  $id = $_REQUEST['id'];
   $usuario = $_REQUEST['nome'];
   $email = $_REQUEST['email'];
   $senha = $_REQUEST['senha'];
 
-  if ($id == NULL) {
-    $conexao->query("UPDATE usuario SET nome = '$usuario', email = '$email', senha = '$senha' WHERE id = $id");
-    header('Location: index.php');
-  }
-  
-
   $conexao->query('INSERT INTO usuario (nome, email, senha) VALUES ("'.$usuario.'", "'.$email.'", "'.$senha.'")');
-
 
   header('Location: index.php');
 ?>
