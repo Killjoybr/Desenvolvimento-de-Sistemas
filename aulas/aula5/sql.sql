5137441278e11760cca3fa448f06b73be9dd4401
Killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-03-31 10:33:19 -0300

    atualizando script do banco

--- aulas/aula5/sql.sql
+++ aulas/aula5/sql.sql
@@ -2,14 +2,23 @@ create database if not exists desenvolvimento;
 
 use desenvolvimento;
 
-create table if not exists usuario( 
-  	id int auto_increment not null primary key,
-  	nome varchar(150) not null,
-  	email varchar(150) not null,
-  	senha varchar(100) not null
+create table if not exists usuario(
+	id int auto_increment not null primary key,
+	nome varchar(150) not null,
+	email varchar(150) not null,
+	senha varchar(100) not null
+);
+
+create table if not exists usuario_cargo(
+	id int auto_increment not null primary key,
+	descricao varchar(100) not null
 );
 
 alter table usuario
-add unique (email), add unique (ID);
+add unique (email), add unique (ID); 
 
--- TODO: Implementar atualizacao da tabela usuario p/  
+alter table usuario
+add column cargo int;
+
+alter table usuario
+add foreign key (cargo) references usuario_cargo (id);
