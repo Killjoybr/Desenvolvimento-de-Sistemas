d8c0a69ecc78bcd7c700c8f572a6f0d9ecee6f6d
Killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-04-01 17:08:18 -0300

    arruma indentacoes no html e adiciona ancora de login

--- aulas/aula5/index.php
+++ aulas/aula5/index.php
@@ -37,34 +37,36 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css"> -->
     <!-- <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/milligram/1.4.1/milligram.min.css"> -->
-</head>
-    <table>
+  </head>
+    <body>
+      <a href="autenticacao.php">Login</a>
+      <table>
         <caption><h1>Tabela de Usuarios<strong></caption>
         <thead>
-            <tr>
-                <th>ID</th>
-                <th>Nome</th>
-                <th>E-mail</th>
-                <th>Senha</th>
-                <th>Cargo</th>
-                <th>Ações</th>
-            </tr>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>E-mail</th>
+            <th>Senha</th>
+            <th>Cargo</th>
+            <th>Ações</th>
+          </tr>
         </thead>
         <?php  foreach ($usuarios as $usuario):?>
-                    <tr>
-                        <td><?= $usuario['id'] ?></td>
-                        <td><?= $usuario['nome'] ?></td>
-                        <td><?= $usuario['email'] ?></td>
-                        <td><?= $usuario['senha'] ?></td>
-                        <td><?= empty($cargosArr[$usuario['cargo'] - 1]) ? NULL : $cargosArr[$usuario['cargo'] - 1] ?></td>
-                        <td>
-                            <a href="excluir.php?id=<?= $usuario['id'] ?>"> <i class="fa fa-trash"></i> </a>
-                            <a href="editar.php?id=<?= $usuario['id'] ?>"> <i class="fa fa-pencil"></i> </a>
-                        </td>                   
-                    </tr>
+          <tr>
+            <td><?= $usuario['id'] ?></td>
+            <td><?= $usuario['nome'] ?></td>
+            <td><?= $usuario['email'] ?></td>
+            <td><?= $usuario['senha'] ?></td>
+            <td><?= empty($cargosArr[$usuario['cargo'] - 1]) ? NULL : $cargosArr[$usuario['cargo'] - 1] ?></td>
+            <td>
+              <a href="excluir.php?id=<?= $usuario['id'] ?>"> <i class="fa fa-trash"></i> </a>
+              <a href="editar.php?id=<?= $usuario['id'] ?>"> <i class="fa fa-pencil"></i> </a>
+            </td>                   
+          </tr>
         <?php endforeach; ?>
-    </table>
-    <form method="post" action="cadastro.php">
+      </table>
+      <form method="post" action="cadastro.php">
       <h2>Cadastro de Usuário</h2>
       <input type="text" placeholder="Jõaozinho da Goiaba" name="nome" required>
       <br><br>
@@ -72,13 +74,14 @@
       <br><br>
       <input type="password" placeholder="senha" name="senha" required>
       <br><br>
-      <select name="cargo">
-      <option></option>
-      <?php  foreach ($cargosCombo as $cargo): ?>
-        <option label=<?= $cargo['Descricao'] ?>><?= $cargo['id'] ?></option>
-      <?php endforeach; ?>
-      </select>
-      <br><br>
-      <input type="submit" value="Cadastrar" name="cadastrar">
-    </form>
+        <select name="cargo">
+          <option></option>
+          <?php  foreach ($cargosCombo as $cargo): ?>
+          <option label=<?= $cargo['Descricao'] ?>><?= $cargo['id'] ?></option>
+          <?php endforeach; ?>
+          </select>
+        <br><br>
+        <input type="submit" value="Cadastrar" name="cadastrar">
+      </form>
+    </body>
 </html>
