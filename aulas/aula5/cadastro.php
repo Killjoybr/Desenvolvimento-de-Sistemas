e1bf5a514e9decf34cd955fa1703cb5dd7eadf8f
Killjoybr <112730142+Killjoybr@users.noreply.github.com>
2025-04-01 16:44:50 -0300

    adiciona hash BCRYPT e ternario no campo cargo

--- aulas/aula5/cadastro.php
+++ aulas/aula5/cadastro.php
@@ -4,14 +4,16 @@
   $usuario = $_REQUEST['nome'];
   $email = $_REQUEST['email'];
   $senha = $_REQUEST['senha'];
-  $cargo = $_REQUEST['cargo'];
+  $cargo = $_REQUEST['cargo'] ? $_REQUEST['cargo'] : NULL;
+  
+  $hash = password_hash($senha, PASSWORD_BCRYPT);
 
   $sql = "INSERT INTO usuario (nome, email, senha, cargo) VALUES (:usuario, :email, :senha, :cargo)";
 
   $statement = $conexao->prepare($sql);
   $statement->bindParam(':usuario',$usuario);
   $statement->bindParam(':email',$email);
-  $statement->bindParam(':senha',$senha);
+  $statement->bindParam(':senha',$hash);
   $statement->bindParam(':cargo',$cargo);
 
   $statement->execute();
